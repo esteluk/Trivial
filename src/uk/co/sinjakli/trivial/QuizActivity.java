4a90e298ef29a6332524e199bfa674d99841d0a0
Nathan Wong <nathan@nathanwong.co.uk>
2011-10-08 15:31:41 +0100

    Add options menu, hardware acceleration

--- src/uk/co/sinjakli/trivial/QuizActivity.java
+++ src/uk/co/sinjakli/trivial/QuizActivity.java
@@ -32,6 +32,9 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -119,6 +122,25 @@ public class QuizActivity extends Activity {
 		outState.putInt("incorrectAnswers", incorrectAnswers);
 		outState.putInt("numberOfQuestions", numberOfQuestions);
 		super.onSaveInstanceState(outState);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle item selection
+		switch(item.getItemId()) {
+		case R.id.menu_restart:
+			finish();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 
 	/**
